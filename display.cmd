f028786e2dd1f62378ad69b85b7883008f9146fc
Jason <clock8564@gmail.com>
2023-07-20 09:42:07 +0800

    'update'

--- display.cmd
+++
@@ -1,1 +0,0 @@
-python emotions.py --mode display
---
+++ main.py
@@ -0,0 +1,114 @@
+import argparse
+import cv2
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Environment preset
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['CUDA_VISIBLE_DEVICES']='2, 3'
+
+# Training Detail
+dir_path = {'train': "data/train", 'test': "data/test"}
+num_train = 29690
+num_val = 8187
+batch_size = 64
+num_epoch = 100
+
+# Inference Detail
+emotion_label = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+face_score = 0
+point = {
+    0:3,
+    1:1,
+    2:-3,
+    3:-5,
+    4:5,
+    5:-1,
+    6:1
+}
+
+def GetDataset(dirPath: dict):
+    # Define data generators
+    train_datagen = ImageDataGenerator(rescale=1./255)
+    val_datagen = ImageDataGenerator(rescale=1./255)
+    train_generator = train_datagen.flow_from_directory(
+            dir_path['train'],
+            target_size=(48,48),
+            batch_size=batch_size,
+            color_mode="grayscale",
+            class_mode='categorical')
+    validation_generator = val_datagen.flow_from_directory(
+            dir_path['test'],
+            target_size=(48,48),
+            batch_size=batch_size,
+            color_mode="grayscale",
+            class_mode='categorical')
+    return train_generator, validation_generator
+
+# Create the model
+def Network():
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(8, activation='softmax'))
+    return model 
+
+def Train(model,train_generator):
+    model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
+    model_info = model.fit_generator(
+            train_generator,
+            steps_per_epoch=num_train // batch_size,
+            epochs=num_epoch,
+            validation_data=validation_generator,
+            validation_steps=num_val // batch_size)
+    model.save_weights('models/model.h5')
+    plot_model_history(model_info)
+
+def RealtimeDisplay(model):
+    model.load_weights('models/model.h5')
+    facecasc = cv2.CascadeClassifier('models/haarcascade_frontalface_default.xml')
+    cv2.ocl.setUseOpenCL(False)
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+            prediction = model.predict(cropped_img)
+            max_index = int(np.argmax(prediction))
+            cv2.putText(frame, emotion_dict[max_index], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            face_score += point[max_index]
+
+        cv2.imshow('Webacm', cv2.resize(frame,(800,600),interpolation = cv2.INTER_CUBIC))
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    model = Network()
+    #Train(model)
+    RealtimeDisplay(model)
